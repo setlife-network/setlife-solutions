07f3dfcc4c0c231868174a84f6600d31e5e614fc
vdegandesa <vclafarga@gmail.com>
2019-12-04 18:32:43 -0500

    add header component
    closes #1

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { Button, Navbar, Nav} from 'react-bootstrap'
 
 const Header = (props) => {
     return (
-        <div>
-            Header
+        <div className='row p-3 m-auto col-12 bg-secondary shadow-sm'>
+            <Navbar 
+                className='col-12 col-md-10 text-primary'
+                expand="lg"
+            >
+                <Navbar.Brand href="#home">LafaTech Solutions</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mx-auto ">
+                        <Nav.Link className='mx-3 text-primary' href="#Services">Services</Nav.Link>
+                        <Nav.Link className='mx-3 text-primary' href="#Experience">Experience</Nav.Link>
+                        <Nav.Link className='mx-3 text-primary' href="#Resources">Resources</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+            <div className='col-12 d-flex col-md-2'>
+                <Button className='m-auto'
+                variant="primary">Free Consultation</Button>
+            </div>
+            
         </div>
     )
 }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Jumbotron, Container} from'react-bootstrap';
+
+import Header from '../components/Header';
 
 class HomePage extends React.Component {
     render() {
         return (
-            <div>
-                HomePage
+
+            <div className='m-auto'>
+                <Header/>
+                <Jumbotron fluid>
+                    <Container>
+                        <h1>Engineer Software Analytical Studies</h1>
+                        <p>
+                        We will provide you with the latest technological tools for your upcoming projects
+                        </p>
+                    </Container>
+                </Jumbotron>
+                
             </div>
         );
     }
