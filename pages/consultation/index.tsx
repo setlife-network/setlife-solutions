88551baa6c46a9a69fe26eefb4ac58bbe3822c54
Sofia Rodriguez <sofiaromorales@gmail.com>
2022-07-07 14:39:02 -0400

    feat: Initial src/pages app structure

---
+++ pages/consultation/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const ConsultationPage: NextPage = () => {
+  return (
+    <div>
+      ConsultationPage
+    </div>
+  )
+}
+
+export default ConsultationPage
