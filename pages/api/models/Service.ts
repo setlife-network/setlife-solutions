20294d3a90f5f143c3aa72b66681e4d4233bb84d
Sofia Rodriguez <sofiaromorales@gmail.com>
2022-07-16 23:10:04 -0400

    feat: Service model created

---
+++ pages/api/models/Service.ts
@@ -0,0 +1,32 @@
+const { DataTypes, Model } = require('sequelize')
+
+module.exports = (sequelize: any) => {
+
+    class Service extends Model {}
+
+    Service.init({
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    },
+    {
+        sequelize,
+        modelName: 'Service',
+        createdAt: 'created_at',
+        updatedAt: 'updated_at'
+    })
+
+    return Service
+
+}
