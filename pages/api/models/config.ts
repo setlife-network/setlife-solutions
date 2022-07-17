dbb06e0d4fa4456e0cecfd413aea96d47fbaa247
Sofia Rodriguez <sofiaromorales@gmail.com>
2022-07-16 23:18:26 -0400

    sequelize cli config

---
+++ pages/api/models/config.ts
@@ -0,0 +1,40 @@
+const { POSTGRES } = require('../../../config/credentials')
+
+const {
+    DB_HOST,
+    DB_USERNAME,
+    DB_PASSWORD,
+    DB_NAME,
+    DB_PORT
+} = POSTGRES
+
+module.exports = {
+    development: {
+        username: DB_USERNAME,
+        password: DB_PASSWORD,
+        database: DB_NAME,
+        host: DB_HOST,
+        dialect: 'postgres'
+    },
+    staging: {
+        username: DB_USERNAME,
+        password: DB_PASSWORD,
+        database: DB_NAME,
+        host: DB_HOST,
+        dialect: 'postgres'
+    },
+    test: {
+        username: DB_USERNAME,
+        password: DB_PASSWORD,
+        database: DB_NAME,
+        host: DB_HOST,
+        dialect: 'postgres'
+    },
+    production: {
+        username: DB_USERNAME,
+        password: DB_PASSWORD,
+        database: DB_NAME,
+        host: DB_HOST,
+        dialect: 'postgres'
+    }
+}
