a314d605578819071881a2244f436f68a9294c67
Ovidio Rodriguez <ovifer02@gmail.com>
2022-07-18 11:39:34 -0400

    Create project model

---
+++ pages/api/models/Project.ts
@@ -0,0 +1,48 @@
+const { DataTypes, Model } = require('sequelize')
+
+module.exports = (sequelize: any) => {
+
+    class Project extends Model {}
+
+    Project.init({
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        client_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        client_description: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        project_resource: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        other_resource: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    },
+    {
+        sequelize,
+        modelName: 'Project',
+        createdAt: 'created_at',
+        updatedAt: 'updated_at'
+    })
+
+    return Project
+
+}
