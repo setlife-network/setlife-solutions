103f9da7809ddcc4e48bea9ab62fad4507aa6255
Ovidio Rodriguez <ovifer02@gmail.com>
2022-07-27 09:42:34 -0400

    fetchProject query

--- pages/api/graphql/schema/resolvers/ProjectResolver.ts
+++ pages/api/graphql/schema/resolvers/ProjectResolver.ts
@@ -6,6 +6,9 @@ module.exports = {
     Query: {
         fetchProjects: (project: any, args: any, db: any) => (
             db.models.Project.findAll()
+        ),
+        fetchProject: (project: any, args:any, db: any) => (
+            db.models.Project.findByPk(args.id)
         )
     },
   
