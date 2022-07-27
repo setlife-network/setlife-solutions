1c2ff1958d92762b05d25f0a93eb293b2baa1184
Ovidio Rodriguez <ovifer02@gmail.com>
2022-07-27 11:56:59 -0400

    Add serviceDetails field

--- pages/api/graphql/schema/resolvers/ServiceResolver.ts
+++ pages/api/graphql/schema/resolvers/ServiceResolver.ts
@@ -1,6 +1,9 @@
 module.exports = {
 
     Service: {
+        serviceDetails: (service: any, args:any, { ServiceDetail }: any) => (
+            ServiceDetail.findAll({ where: { service_id: service.id }})
+        )
     },
 
     Query: {
