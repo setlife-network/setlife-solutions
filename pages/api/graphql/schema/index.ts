9709d8abf463cbc5ada06e9d8c2b0c57f2434a55
Ovidio Rodriguez <ovifer02@gmail.com>
2022-07-27 08:08:27 -0400

    Import service type and resolver

--- pages/api/graphql/schema/index.ts
+++ pages/api/graphql/schema/index.ts
@@ -2,18 +2,22 @@ const { makeExecutableSchema } = require('@graphql-tools/schema');
 const { mergeTypeDefs, mergeResolvers } = require('@graphql-tools/merge')
 
 // Import types
+const ServiceType = require('./types/ServiceType')
 const TestType = require('./types/TestType')
 
 // Import resolvers
+const ServiceResolver = require('./resolvers/ServiceResolver')
 const TestResolver = require('./resolvers/TestResolver')
 
 // Merge types
 const typeDefs = mergeTypeDefs([
+    ServiceType,
     TestType
 ])
 
 // Merge resolvers
 const resolvers = mergeResolvers([
+    ServiceResolver,
     TestResolver
 ])
 
