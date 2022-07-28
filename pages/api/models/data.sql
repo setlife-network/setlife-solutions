ae42f35fc5315b37c03130a75ffcc39af37bc1b7
Sofia Rodriguez <sofiaromorales@gmail.com>
2022-07-28 14:01:06 -0400

    feat: Added service inserts

---
+++ pages/api/models/data.sql
@@ -0,0 +1,8 @@
+-- Services
+INSERT INTO public.services
+VALUES 
+    (1, 'Software Consulting', 'This is the best place to start if you just need some basic technical advisement on how to get your project going', '2022-07-28', '2022-07-28'),
+    (2, 'Project Engineering Management', 'With a Software Solutions Engineer at the helm, you’ll have weekly updates and a high-level view of how your project is going', '2022-07-28', '2022-07-28'),
+    (3, 'Product Design', 'Your project deserves a strong design phase to produce wireframes & flowcharts outlining UIUX & technology architecture', '2022-07-28', '2022-07-28'),
+    (4, 'Software Implementation', 'Here we handle the heavy lifting! After coding & deploying your solution, you will finally see your project brought to life!', '2022-07-28', '2022-07-28'),
+    (5, 'Application Maintenance', 'Once your solution is live, we’ll make sure to monitor, support, and maintain your system with production-grade service & response', '2022-07-28', '2022-07-28');
