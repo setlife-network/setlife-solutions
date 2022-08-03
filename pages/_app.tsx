07f2fdbf14d43e3b3b69c7b9d45fbfc50b215be5
Ovidio Rodriguez <ovifer02@gmail.com>
2022-08-03 13:40:04 -0400

    Import layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,18 @@
 import type { AppProps } from 'next/app'
 import { ApolloProvider } from '@apollo/client'
 import client from '../config/apollo-client'
+import Layout from '../components/layout'
 
 import '../styles/index.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         <ApolloProvider client={client}>
-            <div className='default-theme'>
-                <Component {...pageProps} />
-            </div>
+            <Layout>
+                <div className='default-theme'>
+                    <Component {...pageProps} />
+                </div>
+            </Layout>
         </ApolloProvider>
     )
 }
