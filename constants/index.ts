9e2f04b3bed619a7d547af2463ee212933bcaccf
Ovidio Rodriguez <ovifer02@gmail.com>
2022-08-05 09:56:49 -0400

    add constants

--- constants/index.ts
+++ constants/index.ts
@@ -1,2 +1,5 @@
 export const API_v1_URL: string = 'http://localhost:3000/api/v1'
 export const LOGO_URL: string = 'https://setlife-solutions.s3.amazonaws.com/images/Logo.png'
+export const NAV_ITEMS: Array<string> = ['About', 'Portfolio', 'Consultation', 'Services', 'Service Packages']
+export const SETLIFE: string = 'setlife'
+export const SOLUTIONS: string = 'solutions'
