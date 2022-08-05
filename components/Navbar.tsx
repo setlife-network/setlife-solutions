6a2ea12c6c58be4c2bdcb9383f6fcdbf2f938e74
Sofia Rodriguez <sofiaromorales@gmail.com>
2022-08-04 23:47:48 -0400

    code styling fixes

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,14 +3,20 @@ import { useState } from 'react'
 import { LOGO_URL } from '../constants'
 
 function NavLink({to, children}: any) {
-    return <a href={to} className={`mx-4`}>
-        {children}
-    </a>
+    return (
+        <a href={to} className='mx-4'>
+            {children}
+        </a>
+    )
 }
 
 function MobileNav({open, setOpen}: any) {
     return (
-        <div className={`absolute top-0 bg-solid-white left-0 h-screen w-screen transform ${open ? "-translate-x-0" : "-translate-x-full"} transition-transform duration-300 ease-in-out filter drop-shadow-md `}>
+        <div 
+            className={`
+                absolute top-0 bg-solid-white left-0 h-screen w-screen transform ${open ? '-translate-x-0' : '-translate-x-full'} transition-transform duration-300 ease-in-out filter drop-shadow-md
+            `}
+        >
             <div className="flex items-center justify-center filter drop-shadow-md bg-solid-black h-20">
                 <a href="/">
                     <img 
@@ -45,7 +51,7 @@ export default function Navbar() {
     const [open, setOpen] = useState(false)
     return (
         <nav className="flex filter drop-shadow-md bg-solid-black px-4 py-4 h-20 items-center">
-            <MobileNav open={open} setOpen={setOpen}/>
+            <MobileNav open={open} setOpen={setOpen} />
             <div className="w-3/12 flex items-center">
                 <a href="/">
                 <img 
@@ -58,10 +64,12 @@ export default function Navbar() {
                 <p className='font-semibold text-2xl text-solid-white'>setlife</p><p className='text-2xl font-thin text-primary'>solutions</p>
             </div>
             <div className="w-3/12 flex justify-end items-center">
-
-                <div className="z-50 flex relative w-8 h-8 flex-col justify-between items-center" onClick={() => {
-                    setOpen(!open)
-                }}>
+                <div 
+                    className="z-50 flex relative w-8 h-8 flex-col justify-between items-center" 
+                    onClick={() => {
+                        setOpen(!open)
+                    }}
+                >
                     {/* hamburger button */}
                     <span className={`h-0.5 w-full bg-solid-white rounded-lg transform transition duration-300 ease-in-out ${open ? "rotate-45 translate-y-3.5" : ""}`} />
                     <span className={`h-0.5 w-full bg-solid-white rounded-lg transition-all duration-300 ease-in-out ${open ? "w-0" : "w-full"}`} />
--- components/layout.tsx
+++ components/layout.tsx
@@ -1,10 +1,12 @@
-import Navbar from "./Navbar"
+import Navbar from './Navbar'
 
 export default function Layout({ children }: any) {
     return (
         <>
             <Navbar/>
-            <main>{children}</main>
+            <main>
+                { children }
+            </main>
         </>
     )
 }
