bde5d959887927793e1217e4f7dbc0ca739688b3
Ovidio Rodriguez <ovifer02@gmail.com>
2022-08-09 14:55:07 -0400

    Import bitcoin image as a background

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        'primary': '#00C2D4',
-        'primary-alt': 'rgba(0, 194, 212, 0.7)',
-        'solid-black': '#000000',
-        'solid-white': '#FFFFFF',
-        'light-gray': '#F2F2F2'
-      }
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx}',
+        './components/**/*.{js,ts,jsx,tsx}',
+    ],
+    theme: {
+        extend: {
+            backgroundImage: {
+                'bitcoin': 'url("https://setlife-solutions.s3.amazonaws.com/images/B+bitcoin.png")'
+            },
+            colors: {
+                'primary': '#00C2D4',
+                'primary-alt': 'rgba(0, 194, 212, 0.7)',
+                'solid-black': '#000000',
+                'solid-white': '#FFFFFF',
+                'light-gray': '#F2F2F2'
+            }
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
