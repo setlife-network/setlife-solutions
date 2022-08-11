83d1b4c357464d0dcaa7c438c870a9c03e48aae1
Ovidio Rodriguez <ovifer02@gmail.com>
2022-08-11 16:40:58 -0400

    Import logos and array

--- constants/index.ts
+++ constants/index.ts
@@ -11,5 +11,11 @@ export const API_v1_URL: string = 'http://localhost:3000/api/v1'
 // Images
 export const HERO_IMAGE_URL = 'https://user-images.githubusercontent.com/49292858/183115188-87aa75f2-7123-472a-b20d-16766828f96c.png'
 export const LOGO_URL: string = 'https://setlife-solutions.s3.amazonaws.com/images/Logo.png'
+export const GITHUB_LOGO: string = 'https://setlife-solutions.s3.amazonaws.com/images/Github.png'
+export const LINKEDIN_LOGO: string = 'https://setlife-solutions.s3.amazonaws.com/images/Linkedin.png'
+export const TWITTER_LOGO: string = 'https://setlife-solutions.s3.amazonaws.com/images/Twitter.png'
+export const YOUTUBE_LOGO: string = 'https://setlife-solutions.s3.amazonaws.com/images/Youtube.png'
 
+// Arrays
 export const NAV_ITEMS: Array<string> = [ABOUT, PORTFOLIO, CONSULTATION, SERVICES, SERVICE_PACKAGES]
+export const FOOT_ITEMS: Array<string> = [GITHUB_LOGO, LINKEDIN_LOGO, TWITTER_LOGO, YOUTUBE_LOGO]
