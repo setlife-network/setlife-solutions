cd8f150e8e9911c27a22dd7f2d919dc2143c7fb0
Ovidio Rodriguez <ovifer02@gmail.com>
2022-08-11 11:45:23 -0400

    Footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = ({}) => {
+    return (
+        <footer className='bg-solid-black'>
+            <div className='grid grid-cols-1 mx-8 md:mx-24 xl:mx-48'>
+                <div>
+                    <h1 className='text-solid-white'>Footer</h1>
+                </div>
+            </div>
+        </footer>
+    )
+
+}
+
+export default Footer
