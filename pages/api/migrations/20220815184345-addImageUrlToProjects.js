1c8e638aec61ad7b94cdfd1a593aa40e4d15566a
Ovidio Rodriguez <ovifer02@gmail.com>
2022-08-15 15:10:08 -0400

    addImageUrl migration

---
+++ pages/api/migrations/20220815184345-addImageUrlToProjects.js
@@ -0,0 +1,22 @@
+module.exports = {
+
+    async up (queryInterface, Sequelize) {
+        return queryInterface.sequelize.transaction(t => {
+            return Promise.all([
+                queryInterface.addColumn('projects', 'image_url', {
+                    type: Sequelize.DataTypes.STRING,
+                    allowNull: false
+                }, { transaction: t })
+            ])
+        })
+    },
+
+    async down (queryInterface, Sequelize) {
+        return queryInterface.sequelize.transaction(t => {
+            return Promise.all([
+                queryInterface.removeColumn('projects', 'image_url', { transaction: t })
+            ])
+        })
+    }
+
+}
