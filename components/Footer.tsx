04ba721ee527766e033770bcf3202ae17722877c
Ovidio Rodriguez <ovifer02@gmail.com>
2022-08-15 13:12:16 -0400

    Assign urls

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,21 +1,41 @@
 import React from 'react'
 import Headline from './Headline'
 
-import { FOOT_ITEMS } from '../constants'
+import { 
+    FOOT_ITEMS,
+    GITHUB_URL,
+    LINKEDIN_URL,
+    SETLIFE_NETWORK_URL,
+    TWITTER_URL,
+    YOUTUBE_URL,
+} from '../constants'
 import {
     SETLIFE,
     SOLUTIONS,
     SETLIFE_SOLUTIONS_COPYRIGHT,
-    SETLIFE_NETWORK
+    SETLIFE_NETWORK,
 } from '../constants/strings'
 
 const renderFootItems = () => {
     return (
         FOOT_ITEMS.map((n, i) => {
             const logoName = n.substring(50).replace('.png', '')
+            const URL = () => {
+                if (logoName == 'Youtube') {
+                    return YOUTUBE_URL
+                } else if (logoName == 'Twitter') {
+                    return TWITTER_URL
+                } else if (logoName == 'Github') {
+                    return GITHUB_URL
+                } else if (logoName == 'Linkedin') {
+                    return LINKEDIN_URL
+                }
+            }
             return (
                 <a 
-                    href='/' 
+                    href={URL()}
+                    rel='noopener noreferrer'
+                    target='_blank'
                     key={i}
                     className='mx-8 self-center md:mx-6'
                 >
@@ -34,7 +54,13 @@ const Footer = ({}) => {
                 <Headline variant='h2-thin' color='primary'>{ SOLUTIONS }</Headline>
             </div>
             <div className='w-full flex justify-center mb-5 md:hidden'>
-                <Headline variant='h2-semibold' color='solid-white'>{ SETLIFE_NETWORK }</Headline>
+                <a 
+                    href={SETLIFE_NETWORK_URL}
+                    rel='noopener noreferrer'
+                    target='_blank'
+                >
+                    <Headline variant='h2-semibold' color='solid-white'>{ SETLIFE_NETWORK }</Headline>
+                </a>
             </div>
             <div className='hidden md:w-8/12 md:ml-12 md:mt-2 md:flex'>
                 <Headline variant='xs' color='solid-white'>{ SETLIFE_SOLUTIONS_COPYRIGHT }</Headline>
