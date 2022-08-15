c960495d262adf30b196651c978bc0e694885af6
Ovidio Rodriguez <ovifer02@gmail.com>
2022-08-15 13:11:40 -0400

    Add URLs

--- constants/index.ts
+++ constants/index.ts
@@ -16,6 +16,13 @@ export const LINKEDIN_LOGO: string = 'https://setlife-solutions.s3.amazonaws.com
 export const TWITTER_LOGO: string = 'https://setlife-solutions.s3.amazonaws.com/images/Twitter.png'
 export const YOUTUBE_LOGO: string = 'https://setlife-solutions.s3.amazonaws.com/images/Youtube.png'
 
+// URL
+export const YOUTUBE_URL: string = 'https://www.youtube.com/channel/UCjyzH2XN7qB3WuWTPFbJXCQ'
+export const TWITTER_URL: string = 'https://twitter.com/SetLifeLearning'
+export const GITHUB_URL: string = 'https://github.com/setlife-network'
+export const LINKEDIN_URL: string = 'https://www.linkedin.com/company/setlife-network'
+export const SETLIFE_NETWORK_URL: string = 'https://www.setlife.network/'
+
 // Arrays
 export const NAV_ITEMS: Array<string> = [ABOUT, PORTFOLIO, CONSULTATION, SERVICES, SERVICE_PACKAGES]
 export const FOOT_ITEMS: Array<string> = [GITHUB_LOGO, LINKEDIN_LOGO, TWITTER_LOGO, YOUTUBE_LOGO]
