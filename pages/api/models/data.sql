51b671b9ba4200521f516d9ad3cbcbb30dafdd96
Ovidio Rodriguez <ovifer02@gmail.com>
2022-08-16 12:08:03 -0400

    Add Update query

--- pages/api/models/data.sql
+++ pages/api/models/data.sql
@@ -27,7 +27,7 @@ INSERT INTO public.projects
 VALUES
     (1, 'Careerscore', 'CareerScore, a Job Application Tracking tool, which helps schools measure and improve student job placement', 'Quotanda', 'Quotanda is expanding opportunities for institutions to increase educational access and improve student outcomes.', 'https://www.careerscore.com', null, '2022-07-28', '2022-07-28', 'https://setlife-solutions.s3.amazonaws.com/images/Careerscore.png'),
     (2, 'WorkSTEPS', 'CareerScore, a Job Application Tracking tool, which helps schools measure and improve student job placement', 'Quotanda', 'Quotanda is expanding opportunities for institutions to increase educational access and improve student outcomes.', 'https://www.worksteps.com', null, '2022-07-28', '2022-07-28', 'https://setlife-solutions.s3.amazonaws.com/images/Woksteps.png'),
-    (3, 'Angling Lines', 'CareerScore, a Job Application Tracking tool, which helps schools measure and improve student job placement', 'Quotanda', 'Quotanda is expanding opportunities for institutions to increase educational access and improve student outcomes.', 'https://www.anglinglines.com', '', null, '2022-07-28', '2022-07-28', 'https://setlife-solutions.s3.amazonaws.com/images/Angling_Lines.png'),
+    (3, 'Angling Lines', 'CareerScore, a Job Application Tracking tool, which helps schools measure and improve student job placement', 'Quotanda', 'Quotanda is expanding opportunities for institutions to increase educational access and improve student outcomes.', 'https://www.anglinglines.com', null, '2022-07-28', '2022-07-28', 'https://setlife-solutions.s3.amazonaws.com/images/Angling_Lines.png'),
     (4, 'Compass', 'CareerScore, a Job Application Tracking tool, which helps schools measure and improve student job placement', 'Quotanda', 'Quotanda is expanding opportunities for institutions to increase educational access and improve student outcomes.', 'https://compassmining.io', null, '2022-07-28', '2022-07-28', 'https://setlife-solutions.s3.amazonaws.com/images/Compass.png'),
     (5, 'Guatt', 'CareerScore, a Job Application Tracking tool, which helps schools measure and improve student job placement', 'Guatt', 'Guatt is an early-stage startup building a bitcoin-native digital bank using Galoy`s open-source banking platform.', 'https://guatt.io', null, '2022-07-28', '2022-07-28', 'https://setlife-solutions.s3.amazonaws.com/images/Guatt.png');
 
@@ -66,7 +66,23 @@ VALUES
     (2, 'Preliminary Development Approach', 'This document will provide a structure and foundation for the technical requirements of a project as well as some cost frameworking for how to proceed further with development. For most projects (especially larger ones), the Development Plan should be a living document owned by the client that contains enough of the business requirements and technical details to begin executing on a focused phase of design or engineering.', '$1,000 - $6,000', null, '3 meetings', 'SetLife will dedicate a software solutions engineer to produce a preliminary draft for a Development Plan.', null, '2022-07-28', '2022-07-28'),
     (3, 'Dedicated Solutions Engineering', 'This package has a minimum 3-month term to deliver at least 1 software release that can be deployed & tested against a set of business requirements. An agile development workflow may be established for continuous software release cycles (typically 4-6 weeks) subject to client satisfaction.', null, '$10,000 per month for 3 months', '1 weekly meeting', 'After a clear plan for development has been provided, a full-time effort will be made to begin executing on the proposed solution.', '1 full-time engineering lead or 1 part-time technial PM and 1 full-time engineer', '2022-07-28', '2022-07-28');
   
+-- Add Image URL
+UPDATE public.projects
+SET image_url = 'https://setlife-solutions.s3.amazonaws.com/images/Careerscore.png'
+WHERE id = 1;
 
+UPDATE public.projects
+SET image_url = 'https://setlife-solutions.s3.amazonaws.com/images/Woksteps.png'
+WHERE id = 2;
 
+UPDATE public.projects
+SET image_url = 'https://setlife-solutions.s3.amazonaws.com/images/Angling_Lines.png'
+WHERE id = 3;
 
+UPDATE public.projects
+SET image_url = 'https://setlife-solutions.s3.amazonaws.com/images/Compass.png'
+WHERE id = 4;
 
+UPDATE public.projects
+SET image_url = 'https://setlife-solutions.s3.amazonaws.com/images/Guatt.png'
+WHERE id = 5;
