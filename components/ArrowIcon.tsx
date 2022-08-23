62f90e0d9fffc20fb2f3d4f3878d3d3c5b5e3086
Sofia Rodriguez <sofiaromorales@gmail.com>
2022-08-23 15:35:04 -0400

    closes #128

---
+++ components/ArrowIcon.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ArrowIcon = () => {
+    return (
+        <div className='ArrowIcon'>
+            <svg xmlns='http://www.w3.org/2000/svg' className='inline ml-4 mt-1 h-4 w-4 md:mt-0 md:w-6 md:h-6' fill='none' viewBox='0 0 24 24' stroke='currentColor' strokeWidth={2}>
+                <path strokeLinecap='round' strokeLinejoin='round' d='M14 5l7 7m0 0l-7 7m7-7H3' />
+            </svg>
+        </div>
+    )
+}
+
+export default ArrowIcon
--- pages/projects/[projectId].tsx
+++ pages/projects/[projectId].tsx
@@ -1,12 +1,20 @@
 import type { NextPage } from 'next'
 import { useRouter } from 'next/router'
 
+import ProjectDetailBanner from '../../components/ProjectDetailBanner'
+
 const ProjectDetailPage: NextPage = () => {
     const router = useRouter()
     const { projectId } = router.query
     return (
-        <div>
-      ProjectDetailPage: {projectId}
+        <div className='ProjectDetailPage'>
+            <ProjectDetailBanner
+                backgroundImageUrl={}
+                deviceImageUrl={}
+                logoImageUrl={}
+                projectName={}
+            />
+          
         </div>
     )
 }
