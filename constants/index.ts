bf78caff544cbcc1abbc3e7d2d66f73428121b94
Oscar Lafarga <otech47@gmail.com>
2022-08-24 13:48:18 -0400

    Add fonts (in progress)

--- constants/index.ts
+++ constants/index.ts
@@ -6,7 +6,7 @@ import {
     SERVICES,
 } from './strings'
 
-export const API_v1_URL: string = 'http://localhost:3000/api/v1'
+export const API_v1_URL: string = 'http://localhost:3200/api/v1'
 
 // Images
 export const HERO_IMAGE_URL = 'https://user-images.githubusercontent.com/49292858/183115188-87aa75f2-7123-472a-b20d-16766828f96c.png'
