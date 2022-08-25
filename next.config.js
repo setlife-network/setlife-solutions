1fa1248f6d0e01043f0c1224def575950bc9f8c2
Ovidio Rodriguez <ovifer02@gmail.com>
2022-08-25 17:21:38 -0400

    Change dotenv to next.config file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
+
+require('dotenv').config()
+
 const nextConfig = {
-  reactStrictMode: true,
+    reactStrictMode: true,
+    env: {
+        POSTGRES_DB_HOST: process.env.POSTGRES_DB_HOST,
+        POSTGRES_DB_USERNAME: process.env.POSTGRES_DB_USERNAME,
+        POSTGRES_DB_PASSWORD: process.env.POSTGRES_DB_PASSWORD,
+        POSTGRES_DB_NAME: process.env.POSTGRES_DB_NAME,
+        POSTGRES_DB_PORT: process.env.POSTGRES_DB_PORT,
+        API_V1_URL: process.env.API_V1_URL,
+    }
 }
 
 module.exports = nextConfig
