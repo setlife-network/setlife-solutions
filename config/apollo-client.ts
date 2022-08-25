4f1b6684ce94933b90109dd432dfa527e3644106
Ovidio Rodriguez <ovifer02@gmail.com>
2022-08-25 16:45:26 -0400

    Fix naming

--- config/apollo-client.ts
+++ config/apollo-client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 const client = new ApolloClient({
-    uri: process.env.API_V1_URL,
+    uri: process.env.API_V1_ROUTE,
     cache: new InMemoryCache(),
 });
 
