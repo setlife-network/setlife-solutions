a2963e9e32582e22c7a880b4dfdb890d7a0b6e2e
Sofia Rodriguez <sofiaromorales@gmail.com>
2022-08-26 11:15:13 -0400

    closes #151

--- pages/consultation/index.tsx
+++ pages/consultation/index.tsx
@@ -1,9 +1,16 @@
 import type { NextPage } from 'next'
 
+import Headline from '../../components/Headline'
+import Section from '../../components/Section'
+
 const ConsultationPage: NextPage = () => {
     return (
-        <div>
-      ConsultationPage
+        <div className='ConsultationPage'>
+            <Section>
+                <Headline variant='h1'>
+                    Please fill out the form below to schedule your consultation. 
+                </Headline>
+            </Section>
         </div>
     )
 }
