1cfc2013cd8dc3df5aa879a565ff9917ca03d52b
Sofia Rodriguez <sofiaromorales@gmail.com>
2022-08-29 18:35:43 -0400

    map for rendering inputs contact info

--- components/ContactInformation.tsx
+++ components/ContactInformation.tsx
@@ -26,45 +26,47 @@ const ContactInformation = ({
         })
     }, [name, email, phoneNumber])
 
+    const renderInputs = () => {
+        const basicInformationFields = [
+            {
+                name: NAME,
+                onChange: setName
+            },
+            {
+                name: EMAIL,
+                onChange: setEmail
+            },
+            {
+                name: PHONE_NUMBER,
+                onChange: setPhoneNumber
+            }
+        ]
+        return basicInformationFields.map(input => {
+            return (
+                <label 
+                    className='relative block p-3 border-2 border-primary rounded-full' 
+                    htmlFor='name' 
+                    key={input.name}
+                >
+                    <input
+                        className='w-full px-4 pt-3.5 pb-0 text-sm placeholder-transparent border-none focus:ring-0 peer'
+                        id={input.name}
+                        type='text'
+                        placeholder={input.name}
+                        onChange={(e) => input.onChange(e.target.value)}
+                    />
+                    <span className='absolute text-xs px-4 font-medium text-gray-500 transition-all left-3 peer-focus:text-xs peer-focus:top-3 peer-focus:translate-y-0 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:text-sm'>
+                        { input.name }
+                    </span>
+                </label>
+            )
+        })
+    }
+
     return (
         <div className='ContactInformation'>
             <div className='grid grid-flow-row auto-rows-max gap-8 w-full md:w-6/12'>
-                <label className='relative block p-3 border-2 border-primary rounded-full' htmlFor='name'>
-                    <input
-                        className='w-full px-4 pt-3.5 pb-0 text-sm placeholder-transparent border-none focus:ring-0 peer'
-                        id='name'
-                        type='text'
-                        placeholder={NAME}
-                        onChange={(e) => setName(e.target.value)}
-                    />
-                    <span className='absolute text-xs px-4 font-medium text-gray-500 transition-all left-3 peer-focus:text-xs peer-focus:top-3 peer-focus:translate-y-0 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:text-sm'>
-                        {NAME}
-                    </span>
-                </label>
-                <label className='relative block p-3 border-2 border-primary rounded-full' htmlFor='name'>
-                    <input
-                        className='w-full px-4 pt-3.5 pb-0 text-sm placeholder-transparent border-none focus:ring-0 peer'
-                        id='email'
-                        type='text'
-                        placeholder={EMAIL}
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                    <span className='absolute text-xs px-4 font-medium text-gray-500 transition-all left-3 peer-focus:text-xs peer-focus:top-3 peer-focus:translate-y-0 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:text-sm'>
-                        {EMAIL}
-                    </span>
-                </label>
-                <label className='relative block p-3 border-2 border-primary rounded-full' htmlFor='name'>
-                    <input
-                        className='w-full px-4 pt-3.5 pb-0 text-sm placeholder-transparent border-none focus:ring-0 peer'
-                        id='Phone number'
-                        type='text'
-                        placeholder={PHONE_NUMBER}
-                        onChange={(e) => setPhoneNumber(e.target.value)}
-                    />
-                    <span className='absolute text-xs px-4 font-medium text-gray-500 transition-all left-3 peer-focus:text-xs peer-focus:top-3 peer-focus:translate-y-0 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:text-sm'>
-                        {PHONE_NUMBER}
-                    </span>
-                </label>
+                { renderInputs() }
             </div>
         </div>
     )
