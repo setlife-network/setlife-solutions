6c70c93df1bae2468abe1020b3d4fcc87ea931c1
Ovidio Rodriguez <ovifer02@gmail.com>
2022-08-31 16:02:58 -0400

    Import GoBack component to layout

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,10 +1,23 @@
+import { useRouter } from 'next/router'
 import Navbar from './Navbar'
 import Footer from './Footer'
+import Section from './Section'
+import GoBack from './GoBack'
 
 export default function Layout({ children }: any) {
+
+    const router = useRouter()
+    
     return (
         <>
             <Navbar />
+            {!(router.pathname == '/' || router.pathname == '/projects') &&
+                (
+                    <Section color='light-gray' paddingBottom='pb-6' paddingTop='pt-6'>
+                        <GoBack router={router.pathname} />
+                    </Section>
+                )
+            }
             <main>
                 { children }
             </main>
