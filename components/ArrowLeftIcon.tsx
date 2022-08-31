dfc976b1b40219377e94e040c2cd65639cf83e7e
Ovidio Rodriguez <ovifer02@gmail.com>
2022-08-31 16:02:00 -0400

    ArrowLeftIcon component

---
+++ components/ArrowLeftIcon.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ArrowLeftIcon = () => {
+    return (
+        <div className='ArrowLeftIcon'>
+            <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='currentColor' className='inline w-6 h-6'>
+                <path strokeLinecap='round' strokeLinejoin='round' d='M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18' />
+            </svg>
+        </div>
+    )
+}
+
+export default ArrowLeftIcon
