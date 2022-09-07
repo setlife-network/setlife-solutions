1b3344126813c0739c7dd1f398913c8ba4293e1a
Ovidio Rodriguez <ovifer02@gmail.com>
2022-09-07 15:30:21 -0400

    Fix GoBack logic

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,4 +1,6 @@
+import React from 'react'
 import { useRouter } from 'next/router'
+
 import Navbar from './Navbar'
 import Footer from './Footer'
 import Section from './Section'
@@ -7,17 +9,23 @@ import GoBack from './GoBack'
 export default function Layout({ children }: any) {
 
     const router = useRouter()
+
+    const renderGoBack = () => {
+        if (!(router.pathname == '/' || router.pathname == '/projects')) {
+            return (
+                <Section color='light-gray' paddingBottom='pb-6' paddingTop='pt-6'>
+                    <GoBack />
+                </Section>
+            )
+        }    
+    }
     
     return (
         <>
             <Navbar />
-            {!(router.pathname == '/' || router.pathname == '/projects') &&
-                (
-                    <Section color='light-gray' paddingBottom='pb-6' paddingTop='pt-6'>
-                        <GoBack router={router.pathname} />
-                    </Section>
-                )
-            }
+            <div className='md:hidden'>
+                { renderGoBack() }
+            </div>
             <main>
                 { children }
             </main>
