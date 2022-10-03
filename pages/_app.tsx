7fd6276d5ef8582d82cd6eb79ac3de87637056c7
Oscar Lafarga <otech47@gmail.com>
2022-10-03 14:21:43 -0400

    Add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,55 @@
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+import Script from 'next/script'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
 import { ApolloProvider } from '@apollo/client'
 import client from '../config/apollo-client'
 import Layout from '../components/layout'
 
+import * as ga from '../utilities/google-analytics'
+
 import '../styles/index.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
+    const router = useRouter()
+
+    useEffect(() => {
+        const handleRouteChange = (url: string) => {
+            ga.pageview(url)
+        }
+        //When the component is mounted, subscribe to router changes
+        //and log those page views
+        router.events.on('routeChangeComplete', handleRouteChange)
+    
+        // If the component is unmounted, unsubscribe
+        // from the event with the `off` method
+        return () => {
+            router.events.off('routeChangeComplete', handleRouteChange)
+        }
+    }, [router.events])
+
     return (
         <>
+            {/* <!-- Google tag (gtag.js) --> */}
+            {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS && (
+                <>
+                    <Script
+                        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+                        strategy="lazyOnload"
+                    />
+                    <Script id="google-analytics" strategy="lazyOnload">
+                        {`
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+
+                            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+                        `}
+                    </Script>
+                </>
+            )}
             <Head>
                 <title>SetLife Solutions</title>
                 <link rel='icon' href='favicon.ico' />
