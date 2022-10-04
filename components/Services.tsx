89fcc50729e15f92f0d46668dcc3f98dfbec90d2
Oscar Lafarga <otech47@gmail.com>
2022-10-04 10:08:57 -0400

    Minor tweaks and polish
    Clean up rendering of services to not be hard-coded
    enable tile image urls

--- components/Services.tsx
+++ components/Services.tsx
@@ -31,22 +31,16 @@ const Services = () => {
 
     const services = [...data!.fetchServices]
 
-    const renderServicesCols = (services: ServiceProps[]) => {
-        return services.map((service, idx) => {
+    const renderServices = () => {
+        return services.map((s, i) => {
             return (
-                <div className={services.length < 3 ? `sm:col-start-${idx + 2 * (idx + 1)} sm:col-span-2` : ''} key={service.id}>
-                    <ServiceTile name={service.name} description={service.description} url={`/services/${service.id}`} />
-                </div>
-            )
-        })
-    }
-
-    const renderServicesRows = (services: ServiceProps[]) => {
-        return services.map((_, idx) => {
-            if ((idx * 3) > services.length - 1) return
-            return (
-                <div className={`grid grid-cols-${(idx * 3) % 2 == 0 ? '3' : '6'} gap-8`}>
-                    { renderServicesCols(services.slice(idx * 3, (idx * 3) + 3)) }
+                <div className='flex flex-initial w-full md:w-1/3 px-4' key={`st-${s.id}`}>
+                    <ServiceTile
+                        name={s.name}
+                        description={s.description}
+                        url={`/services/${s.id}`}
+                        imageUrl={s.tile_image_url}
+                    />
                 </div>
             )
         })
@@ -59,26 +53,8 @@ const Services = () => {
                     { SERVICES }
                 </Headline>
             </div>
-            <div className='grid grid-rows-1 gap-8'>
-                <div className='grid grid-cols-1 lg:grid-cols-3 gap-8'>
-                    <div key={services[0].id}>
-                        <ServiceTile name={services[0].name} description={services[0].description} url={`/services/${services[0].id}`} />
-                    </div>
-                    <div key={services[1].id}>
-                        <ServiceTile name={services[1].name} description={services[1].description} url={`/services/${services[1].id}`} />
-                    </div>
-                    <div key={services[2].id}>
-                        <ServiceTile name={services[2].name} description={services[2].description} url={`/services/${services[2].id}`} />
-                    </div>
-                </div>
-                <div className='grid grid-cols-1 lg:grid-cols-6 gap-8'>
-                    <div className='lg:col-start-2 lg:col-span-2' key={services[3].id}>
-                        <ServiceTile name={services[3].name} description={services[3].description} url={`/services/${services[3].id}`} />
-                    </div>
-                    <div className='lg:col-start-4 lg:col-span-2' key={services[4].id}>
-                        <ServiceTile name={services[4].name} description={services[4].description} url={`/services/${services[4].id}`} />
-                    </div>
-                </div>
+            <div className='flex flex-row flex-wrap justify-center gap-y-8'>
+                {renderServices()}
             </div>
         </div>
     )
