d67294aaa4038b6fa1e4dbdcb0cb57eb3fb1e1a6
Sofia Rodriguez <sofiaromorales@gmail.com>
2022-10-20 12:10:31 -0400

    closes #220

--- next.config.js
+++ next.config.js
@@ -3,12 +3,14 @@
 const nextConfig = {
     reactStrictMode: true,
     env: {
+        API_V1_URL: process.env.API_V1_URL,
         POSTGRES_DB_HOST: process.env.POSTGRES_DB_HOST,
         POSTGRES_DB_USERNAME: process.env.POSTGRES_DB_USERNAME,
         POSTGRES_DB_PASSWORD: process.env.POSTGRES_DB_PASSWORD,
         POSTGRES_DB_NAME: process.env.POSTGRES_DB_NAME,
         POSTGRES_DB_PORT: process.env.POSTGRES_DB_PORT,
-        API_V1_URL: process.env.API_V1_URL,
+        AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+        AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
     }
 }
 
