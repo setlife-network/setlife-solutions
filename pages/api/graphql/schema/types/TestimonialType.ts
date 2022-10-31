5703e809fd6dc8a2f01a1dbe30f16c04218a1869
Ovidio Rodriguez <ovifer02@gmail.com>
2022-10-31 11:22:40 -0400

    Delete unused variables

--- pages/api/graphql/schema/types/TestimonialType.ts
+++ pages/api/graphql/schema/types/TestimonialType.ts
@@ -5,10 +5,8 @@ module.exports = gql`
   type Testimonial {
     id: Int
     person_name: String
-    company_name: String
     testimony: String
     person_image_url: String
-    company_image_url: String
     project: [Project]
   }
 
