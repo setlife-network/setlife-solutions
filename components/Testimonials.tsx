26ca179b99d7cafb772b1839905d1c5d1decc5ee
vdegandesa <vclafarga@gmail.com>
2022-11-10 14:22:47 -0500

    Added more styling to Testimonials and TestimonialsTile

--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -33,7 +33,7 @@ const Testimonials = () => {
                 <TestimonialsTile
                     person_name={t.person_name}
                     testimony={t.testimony}
-                    person_image_url={`/testimonials/${t.id}`}
+                    person_image_url={t.person_image_url}
                     project={t.project}
                 /> 
             </div>
@@ -55,9 +55,5 @@ const Testimonials = () => {
         
     )
 }
-<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
-</svg>
-
 
 export default Testimonials
--- components/TestimonialsTile.tsx
+++ components/TestimonialsTile.tsx
@@ -32,44 +32,40 @@ const TestimonialTile = ({
             //     router.push(url)
             // }}
         >
-            <div className=''>
-                {/* {project
-                    ? (
-                        <div
-                            className='rounded-full bg-cover h-24 w-24 m-auto'
-                            style={{ backgroundImage: `url(${person_image_url})` }}
-                        />
+            
+            
+        <div className='rounded-full bg-contain h-20 w-20 m-auto'
+            style={{ backgroundImage: `url(${person_image_url})` }} 
+        />
+            
+        <div className='flex flex-row'>
+            {project.logo_image_url
+                ? (
+                    <div
+                        className='rounded-full bg-contain h-20 w-20 m-left bg-no-repeat bg-center bg-black'
+                            style={{ backgroundImage: `url(${project.logo_image_url})` }}
+                    />
                     ) : (
                         <div className='rounded-full bg-primary h-24 w-24 m-auto' />
                     )
-                } */}
+                }
+            <div className=''>                
+                <Subtitle variant='m' alignment='text-center' color='solid-black'>
+                    { project.name }
+                </Subtitle>
             </div>
-            
+            </div>     
             <div className=''>
-                <div>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10 m-auto">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
-                </svg>
-
-                </div>
                 <Paragraph alignment='text-center'>
                     { testimony }
                 </Paragraph>
             </div>
-            <div className=''>
-                <Subtitle variant='xs' alignment='text-center'>
-                    { person_name }
-                </Subtitle>
+                <div className=''>
+                    <Subtitle variant='s' alignment='text-center'>
+                        { person_name }
+                    </Subtitle>
+                </div>            
             </div>
-            <div className='text-right'>
-                
-                <Paragraph variant='sm' alignment='text-right' color='primary'>
-                        { project.name }
-                        { project.logo_image_url}
-                </Paragraph>
-                
-            </div>
-        </div>
     )
 }
 
